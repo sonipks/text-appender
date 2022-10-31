ba393e48945540e9b45225e5e623f5f7fad105ce
Pankaj Soni <sonipks@gmail.com>
2022-10-30 23:21:55 -0500

    add main form content

--- src/main/java/com/pks/textappender/App.java
+++ src/main/java/com/pks/textappender/App.java
@@ -12,7 +12,7 @@ public class App extends Application {
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(App.class.getResource("main-view.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 1100, 700);
-        stage.setTitle("Hello!");
+        stage.setTitle("Text Appender!");
         stage.setScene(scene);
         stage.show();
     }
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -9,4 +9,8 @@ module com.pks.textappender {
 
     opens com.pks.textappender to javafx.fxml;
     exports com.pks.textappender;
+    exports com.pks.textappender.controller;
+    opens com.pks.textappender.controller to javafx.fxml;
+    exports com.pks.textappender.helper;
+    opens com.pks.textappender.helper to javafx.fxml;
 }
